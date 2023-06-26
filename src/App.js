29a2f5507558aae64f5e22dad9fe288c76f204d1
MohamedAbdElwahabOka <boka22986@gmail.com>
2023-06-27 02:53:41 +0300

    start Sidebar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
 import "./App.css";
 function App() {
@@ -11,26 +13,62 @@ function App() {
         <div>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
-                    <div className='flex right-4 bottom-4' style={{ zIndex: '1000' }} ></div>
-                    <TooltipComponent content="Settings" position="">
-                        <button type='button' className='text-3xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white'
-                            style={{
-                                background: 'red',
-                                borderRadius: "50%"
-                            }}>
-                            <FiSettings />
-                        </button>
-                    </TooltipComponent>
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }} >
+                        <TooltipComponent
+                            content="Settings"
+                            position="Top"
+                        >
+                            <button type="button" style={{ background: "red", borderRadius: '50%' }}
+                                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray">
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
                     {activeMenu ? (
-                        <div className='w-27 fixed sidebar
-                        dark:bg-secondary-dark-bg'>
-                            sidebar
+                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                            <Sidebar />
                         </div>
                     ) : (
-                        <div>
-                            Routes
+                        <div className='w-0 dark:bg-secondary-dark-bg'>
+                            <Sidebar />
                         </div>
+
                     )}
+                    <div className={
+                        activeMenu ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full" :
+                            "dark:bg-main-dark-bg bg-main-bg min-h-screen w-full flex-2"
+                    }>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            <Navbar />
+                        </div>
+                    </div>
+                    <div>
+                        <Routes>
+                            {/* Dashbord */}
+                            <Route path="/" element={<Ecommerce />} />
+                            <Route path="/Ecommerce" element={<Ecommerce />} />
+                            {/* Orders */}
+                            <Route path="/Orders" element={<Orders />} />
+                            <Route path="/customer" element={<Customers />} />
+                            <Route path="/employees" element={<Employees />} />
+                            {/* apps  */}
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/color-picker" element={<ColorPicker />} />
+
+                            {/* charts  */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
+
+                        </Routes>
+                    </div>
                 </div>
             </BrowserRouter>
         </div>
---
+++ src/pages/ColorPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Colorpicker() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Colorpicker
