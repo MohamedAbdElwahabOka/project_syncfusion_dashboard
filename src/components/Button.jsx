18e244d0d92dfe08d32dfe8e04b05e310db4fa29
MohamedAbdElwahabOka <boka22986@gmail.com>
2023-09-23 13:21:49 +0300

    Ecommerce

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Button({color ,bgColor ,size,text,borderRadius}) {
   return (
-    <button>
-       
+    <button
+    type='button'
+     style ={{backgroundColor :bgColor ,color:color,
+    borderRadius:borderRadius}}
+    className={`text-${size} p-3 hover:drop-shadow-xl`}
+     > 
+       {text} 
     </button>
   )
 }
